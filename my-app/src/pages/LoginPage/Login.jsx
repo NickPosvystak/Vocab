915ed46dc9ee589482bfe494fcfa9cbac845b78d
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2024-04-30 18:20:35 +0200

    Login

--- my-app/src/pages/LoginPage/Login.jsx
+++ my-app/src/pages/LoginPage/Login.jsx
@@ -1,23 +1,52 @@
-export default function Login() {
+import { useForm } from "react-hook-form";
+
+import "./Login.styled.scss";
+import Image from "../../assets/images/illustration.png";
+import { NavLink } from "react-router-dom";
+
+const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
   return (
-    <>
-      <div>
-        <img src="" alt="" />
+    <section className="container">
+      <div className="box-image">
+        <img src={Image} alt="Illustration" className="img-register" />
+        <p className="support-image">Word · Translation · Grammar · Progress</p>
       </div>
-      <div>
-        <h1>Login</h1>
-        <p>
-          To start using our services, please fill out the registration form
-          below. All fields are mandatory:
+
+      <div className="box">
+        <h1 className="title">Login</h1>
+        <p className="description">
+          Please enter your login details to continue using our service:
         </p>
+
+        <div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input
+              type="text"
+              placeholder="Email"
+              {...register("Email", { required: true, pattern: /^\S+@\S+$/i })}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              {...register("Password", { required: true })}
+            />
+
+            <input type="submit" value="Login" />
+            <NavLink to="/register" className="btn-register">
+              Register
+            </NavLink>
+          </form>
+        </div>
       </div>
-      <div>
-        <form action=""></form>
-      </div>
-      <div>
-        <button>Login</button>
-        <button>Register</button>
-      </div>
-    </>
+    </section>
   );
-}
+};
+
+export default Login;
