2751502fe956483ce797c46dbfe9ca28824f7f8a
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2024-05-03 12:24:15 +0200

    Updates

---
+++ static/js/918.025c176b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[918],{3918:(e,a,c)=>{c.r(a),c.d(a,{default:()=>n});c(5043);var s=c(579);const n=()=>(0,s.jsx)("div",{className:"container-rec",children:(0,s.jsx)("h1",{children:"Recommend page"})})}}]);
+//# sourceMappingURL=918.025c176b.chunk.js.map
