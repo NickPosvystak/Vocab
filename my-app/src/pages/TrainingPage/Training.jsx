03fc324c4f8a216f3f753b5757a9ae5b7703204b
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2024-05-21 21:33:41 +0200

    training page

--- my-app/src/pages/TrainingPage/Training.jsx
+++ my-app/src/pages/TrainingPage/Training.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import "./Training.styled.scss";
-import report from "../../assets/images/report.png"
+import report from "../../assets/images/report.png";
 
 const Training = () => {
   return (
     <div className="container-train">
-      <div>
-        <h2>You don't have a single word to learn right now.</h2>
-        <p>
+      <div className="part-one">
+        <h2 className="title-tr">
+          You don't have a single word to learn right now.
+        </h2>
+        <p className="description-tr">
           Please create or add a word to start the workout. We want to improve
           your vocabulary and develop your knowledge, so please share the words
           you are interested in adding to your study.
         </p>
-      <button>Add word</button>
-      <button>Cancel</button>
+        <div className="buttons-box">
+          <button className="btn-tr">Add word</button>
+          <button className="btn-tr-off">Cancel</button>
+        </div>
       </div>
-      <img src={report} alt="image" />
+      <img src={report} alt="report" className="image-tr" />
     </div>
   );
 };
