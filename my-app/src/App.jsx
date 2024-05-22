1f6c77986fb3b9e23b27f2e6d31288ddafc86ce1
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2024-05-22 14:58:50 +0200

    loader

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 import Navigation from "./components/Navigation/navigation";
+import { RotatingLines } from "react-loader-spinner";
 // import CircularProgress from "@mui/material/CircularProgress";
 
 const Home = lazy(() => import("./pages/Home/Home"));
@@ -14,13 +15,27 @@ const TrainingPage = lazy(() => import("./pages/TrainingPage/Training"));
 
 function App() {
   return (
-    
     <div className="App">
       <Navigation />
 
-      <Suspense fallback="Loading....">
+      <Suspense
+        fallback={
+          <div className="loader">
+            <RotatingLines
+              visible={true}
+              height="96"
+              width="96"
+              color="grey"
+              strokeWidth="5"
+              animationDuration="0.75"
+              ariaLabel="rotating-lines-loading"
+              wrapperStyle={{}}
+              wrapperClass=""
+            />
+          </div>
+        }
+      >
         <Routes>
-       
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
